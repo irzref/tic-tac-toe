6c90a45d30c903235226b3b994dc0bc8fe33f9bf
irzref <irzref.92@gmail.com>
2018-02-03 01:24:05 +0100

    update board component

--- app/component/Board.js
+++ app/component/Board.js
@@ -16,21 +16,22 @@ export let Board = (props) => {
   const numOfSquaresInRow = 3;
   const numOfRow = 3;
 
+  const rows = Array(numOfRow).fill(null).map((row, indexOfRow) => {
+    
+    const squares = Array(numOfSquaresInRow).fill(null).map((square, indexOfSquare) => {
+      const i = indexOfSquare + indexOfRow * numOfSquaresInRow;
+      return (renderSquare(i))
+    })
+    
+    return (
+    <div key={indexOfRow.toString()} className={styles.board_row}>      
+      {squares}
+    </div>)
+  }) 
+
   return (
-    <div>
-      
-      {Array(numOfRow).fill(null).map((row, indexOfRow) => {
-        return (
-        <div key={indexOfRow.toString()} className={styles.board_row}>
-          
-          {Array(numOfSquaresInRow).fill(null).map((square, indexOfSquare) => {
-            const i = indexOfSquare + indexOfRow * numOfSquaresInRow;
-            return (renderSquare(i))
-          })}
-
-        </div>)
-      })}
-
+    <div>      
+      {rows}
     </div>
   );
 }
